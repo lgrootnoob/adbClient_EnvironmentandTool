1d94ea8803f35c7d710e492639f46ef6bc34d817
lgrootnoob <jaclane299@gmail.com>
2014-06-10 15:55:34 -0500

    typo

--- bin-3.2.7-Slim/pm.bat
+++ bin-3.2.7-Slim/pm.bat
@@ -3,7 +3,7 @@ rem Below asks for cmd to send to pm on a device shell
 set pom=pm
 echo Enter nothing to go back
 set /p "ppom=Command to send to pm:"
-if not defined ppom goto :doneloop
+if not defined ppom goto :EOF
 set "ayodb=adb shell su -c"
 %ayodb% %pom% %ppom%
 goto :EOF
